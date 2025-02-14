df77f08d1236bc093febb29ff09ae97e9d8730c0
Joelvyr <joelvyrodriguez06@gmail.com>
2025-02-13 20:59:50 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,16 @@ import express, { Express, Request, Response } from "express";
 const app: Express = express();
 const port = 3000;
 
-app.get("/arreglo", (req: Request, res: Response) => {
-  const productos = [
-    { id: 1, nombre: "Laptop", precio: 800 },
-    { id: 2, nombre: "Teléfono", precio: 500 },
-    { id: 3, nombre: "Tablet", precio: 300 },
-  ];
-  res.send({ productos });
-});
+app.get("/Estudiantes", (req: Request, res: Response) => {
 
+  const Estudiantes = [
+    {id: 1, Nombre: "Maria", Curso: "5to", P1: 80, P2: 71, P3: 78, P4: 84},
+    {id: 2, Nombre: "Jacob", Curso: "4to", P1: 79, P2: 80, P3: 98, P4: 87},
+    {id: 3, Nombre: "Pablo", Curso: "3ro", P1: 97,  P2: 100, P3: 90, P4: 100},
+  ];
+  
+  res.send({ Estudiantes });
+});
 
 app.get("/numero", (req: Request, res: Response) => {
   const numero = Math.floor(Math.random() * 100);
