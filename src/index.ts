cbaad6dfc1b87b0468627d517dc63e55d71b97f9
yari3016 <yari22163014@gmail.com>
2025-02-15 14:05:32 -0400

    Llamar las diferentes rutas

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,21 @@
 import express, { Express, Request, Response } from "express";
+import cors from "cors";
 
 const app: Express = express();
 const port = 3000;
 
+app.use(cors<Request>());
+
 app.get("/integer", (req: Request, res: Response) => {
-  res.send(1);
+  res.send({integer:1});
 });
 
 app.get("/string", (req: Request, res: Response) => {
-  res.send("Carlos");
+  res.send({string: "Carlos"});
 });
 
 app.get("/boolean", (req: Request, res: Response) => {
-  res.send(true);
+  res.send({boolean: true});
 });
 
 app.listen(port, () => {
