264404311ba5f49110ab9b4dd377f6d25555128b
yari3016 <yari22163014@gmail.com>
2025-03-27 06:56:46 -0400

    final changes and fixes

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ const sequelize = new Sequelize({
   dialect: 'mysql',
   database: 'tareas',
   username: 'root',
-  password: 'Root1234',
+  password: 'root1234',
   host: 'localhost',
   port: 3306,
   models: [Task],
